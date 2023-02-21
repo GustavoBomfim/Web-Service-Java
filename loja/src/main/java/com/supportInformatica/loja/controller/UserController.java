6005c2887497fe6f23962321f924db94f7656770
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-02-21 14:48:49 -0300

    User entity and controller

---
+++ loja/src/main/java/com/supportInformatica/loja/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.supportInformatica.loja.controller;
+
+import com.supportInformatica.loja.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
+        return ResponseEntity.ok(user);
+    }
+}
