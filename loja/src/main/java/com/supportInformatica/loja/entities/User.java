d9135bd0887686096d00ba87724401dd3626d008
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-02-22 13:49:51 -0300

    create tb_user , h2 database and insert dependency jpa and spring boot tool. '300'

--- loja/src/main/java/com/supportInformatica/loja/entities/User.java
+++ loja/src/main/java/com/supportInformatica/loja/entities/User.java
@@ -1,9 +1,14 @@
 package com.supportInformatica.loja.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table (name = "tb_user")
 public class User implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
