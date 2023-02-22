81956ed4efd65c021db8e4a1560d32c475f91697
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-02-22 14:25:21 -0300

    Create getMapping 'users' and  'users/{id}'

--- loja/src/main/java/com/supportInformatica/loja/controllers/UserController.java
+++ loja/src/main/java/com/supportInformatica/loja/controllers/UserController.java
@@ -1,18 +1,32 @@
 package com.supportInformatica.loja.controllers;
 
 import com.supportInformatica.loja.entities.User;
+import com.supportInformatica.loja.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
-        return ResponseEntity.ok(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
