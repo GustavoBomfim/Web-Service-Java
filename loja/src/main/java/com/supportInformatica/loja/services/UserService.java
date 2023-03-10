5d0c6cfa56e0a43ee92bce32c17fb3a059ff390a
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-03-10 16:07:23 -0300

    User delete

--- loja/src/main/java/com/supportInformatica/loja/services/UserService.java
+++ loja/src/main/java/com/supportInformatica/loja/services/UserService.java
@@ -24,4 +24,7 @@ public class UserService {
     public User insert (User obj){
         return repository.save(obj);
     }
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
